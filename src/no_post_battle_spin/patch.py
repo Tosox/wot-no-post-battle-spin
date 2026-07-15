bee423337ca96e52f4af5b944c28a093ae815e85
Tosox <57193602+Tosox@users.noreply.github.com>
2026-07-15 14:53:56 +0200

    feat: Try settle vehicle before freezing

--- src/no_post_battle_spin/patch.py
+++ src/no_post_battle_spin/patch.py
@@ -1,54 +1,89 @@
 from no_post_battle_spin.log import log
+from no_post_battle_spin.util import safe
 
 
 _METHOD = '_PlayerAvatar__onArenaPeriodChange'
 _oPlayerAvatar__onArenaPeriodChange = None
 
 
+_POLL = 0.05
+_ROT_MIN = 0.1
+_TIMEOUT = 3.0
+_FREEZE_DELAY = 0.25
+
+
+def _is_pivoting(vehicle):
+    veh_filter = vehicle.filter
+    left = veh_filter.leftTrackScroll
+    right = veh_filter.rightTrackScroll
+    return left * right < 0.0 and abs(veh_filter.instantaneousRotationSpeed) > _ROT_MIN
+
+
 def _stop_vehicle(vehicle):
     veh_filter = vehicle.filter
     veh_appearance = vehicle.appearance
 
-    # Stop hull rotation
     veh_filter.reset()
     veh_filter.ignoreInputs = True
-
-    # Stop tracks from moving
-    veh_filter.setTracksSpeed(0.0, True, 0.0, True)
+    veh_filter.setTracksSpeed(0.0, False, 0.0, False)
     veh_appearance.trackScrollController.setData(None)
-
-    # Disable dirt particles thrown by tracks
     veh_appearance.customEffectManager.disableDefaultSelectors(True, True)
 
-    # Stop engine sound
-    vehicle.turnoffThrottle()
+
+def _live_vehicle(vehicle_id):
+    import BigWorld
+    veh = BigWorld.entities.get(vehicle_id)
+    return veh if (veh and veh.isStarted and veh.isAlive()) else None
 
 
-def _stop_all_vehicles(avatar):
+@safe
+def _stop_when_settled(vehicle_id):
     import BigWorld
 
-    for vehicle_id in avatar.arena.vehicles:
-        vehicle = BigWorld.entities.get(vehicle_id)
-        if vehicle and vehicle.isStarted and vehicle.isAlive():
-            _stop_vehicle(vehicle)
+    vehicle = _live_vehicle(vehicle_id)
+    if not vehicle:
+        return
 
-    log('post-battle stop applied')
+    if not vehicle.typeDescriptor.isYawHullAimingAvailable:
+        return
+
+    deadline = BigWorld.time() + _TIMEOUT
+
+    @safe
+    def _apply_stop():
+        veh = _live_vehicle(vehicle_id)
+        if veh:
+            _stop_vehicle(veh)
+
+    @safe
+    def _poll():
+        veh = _live_vehicle(vehicle_id)
+        if not veh:
+            return
+        if _is_pivoting(veh) or BigWorld.time() >= deadline:
+            BigWorld.callback(_FREEZE_DELAY, _apply_stop)
+        else:
+            BigWorld.callback(_POLL, _poll)
+
+    BigWorld.callback(_POLL, _poll)
+
+
+@safe
+def _handle_period_change(avatar, period):
+    from constants import ARENA_PERIOD
+    if period == ARENA_PERIOD.AFTERBATTLE:
+        for vehicle_id in avatar.arena.vehicles:
+            _stop_when_settled(vehicle_id)
 
 
 def _patched_onArenaPeriodChange(self, period, periodEndTime, periodLength, periodAdditionalInfo):
     _oPlayerAvatar__onArenaPeriodChange(self, period, periodEndTime, periodLength, periodAdditionalInfo)
-    try:
-        from constants import ARENA_PERIOD
-        if period == ARENA_PERIOD.AFTERBATTLE:
-            _stop_all_vehicles(self)
-    except Exception:
-        import traceback
-        log('post-battle stop crashed:\n' + traceback.format_exc())
+    _handle_period_change(self, period)
 
 
 def apply_patch():
     global _oPlayerAvatar__onArenaPeriodChange
-    
+
     from Avatar import PlayerAvatar
     if _oPlayerAvatar__onArenaPeriodChange is None:
         _oPlayerAvatar__onArenaPeriodChange = getattr(PlayerAvatar, _METHOD)
